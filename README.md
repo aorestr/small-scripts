5ae815d98169303f265f4ee570240866a9a64501
Ángel Oreste <oresgaze@gmail.com>
2020-06-20 17:19:49 +0200

    creates .bash_profile if it does not exist

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Collection of small scripts I develop for some reason.
 
 Here a list of every script explained:
-* `shell/activate_venv_if_exists.sh`: run it with `sh` and it will add overwrite the `PROMPT_COMMAND` environment variable on the `~/.bash_profile` file. Activates a Python virtual environment if the folder where you are working on your shell contains one. Deactivates it if not. Tested in _Ubuntu 18.04_.
+* `shell/activate_venv_if_exists.sh`: run it with `sh` and it will add overwrite the `PROMPT_COMMAND` environment variable on the `~/.bash_profile` file. Activates a Python virtual environment if the folder where you are working on your shell contains one. Deactivates it if not. The search is done by looking for a folder that ends with _env_ in its name. Tested in _Ubuntu 18.04_.
--- shell/activate_venv_if_exists.sh
+++ shell/activate_venv_if_exists.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 ## Global variables
-THIS_FILE_NAME=$(basename $0)
+THIS_FILE_NAME="$(basename $0)"
 THIS_FILE_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-BASH_PROFILE_PATH=$HOME/.bash_profile
+BASH_PROFILE_PATH="$HOME/.bash_profile"
 
 ## Functions
 add_script_to_bash_profile(){
+  if [ ! -f $BASH_PROFILE_PATH ]; then
+    touch $BASH_PROFILE_PATH
+  fi
   if grep -Fiq "$THIS_FILE_NAME" $BASH_PROFILE_PATH; then
     :
   else
     echo "\nexport PROMPT_COMMAND=\"\\\$(sh $THIS_FILE_PATH/$THIS_FILE_NAME)\"" >> $BASH_PROFILE_PATH
+    exec bash
   fi
 }
 
@@ -20,8 +24,7 @@ check_if_venv_is_activated(){
 }
 
 check_if_venv_exists_in_current_path(){
-  # If the environment variable exists, it shows 1 and 0 otherwise. It searchs for a folder
-  # whose name ends with "env"
+  # It searchs for a folder whose name ends with "env". Echos 1 if there a folder like that and 0 if not
   if ls *env 2>/dev/null | grep -qi pyvenv.cfg; then 
       echo 1 
     else
@@ -41,6 +44,6 @@ else
   if [ "$venv_exists" = "1" ]; then
     # Activate the venv
     venv_path="$(dirname $(find ./ -name 'pyvenv.cfg' -exec readlink -f {} \;))"
-    echo ". $venv_path/bin/activate"
+    echo "source $venv_path/bin/activate"
   fi
 fi
